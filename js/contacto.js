729376e49eadadbb3cce63f37ec19663a9913026
Sandra Sosa <sosasandramabel@gmail.com>
2024-04-26 09:05:10 -0300

    proyecto final

---
+++ js/contacto.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_8f4adip';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Enviar';
+      alert('¡Enviado!');
+      document.getElementById('form').reset();
+    }, (err) => {
+      btn.value = 'Enviar';
+      alert(JSON.stringify(err));
+    });
+});
