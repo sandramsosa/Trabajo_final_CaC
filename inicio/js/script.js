72d8bfb87c04a8ce72b4c1dd7cef4990af39e013
Sandra Sosa <sosasandramabel@gmail.com>
2024-05-10 10:01:54 -0300

    merge

--- inicio/js/script.js
+++
@@ -1,5 +0,0 @@
-document
-  .getElementById("theme-toggle-btn")
-  .addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
-  });
---
+++ js/script.js
@@ -0,0 +1,5 @@
+document
+  .getElementById("theme-toggle-btn")
+  .addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+  });
