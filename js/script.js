19465fc55b5249491cf0f8e228a1e9b5786bcf11
sandramsosa <sosasandramabel@gmail.com>
2024-05-15 17:56:12 -0300

    responsive

--- js/script.js
+++ js/script.js
@@ -5,8 +5,3 @@ document
   });
 
 
-  //menu responsive
-document.getElementById('menu-toggle').addEventListener('click', function() {
-  document.querySelector('nav').classList.toggle('show');
-  this.classList.toggle('show');
-});
